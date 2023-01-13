592bd590234bd47cf19b62bfbcd0998164967109
tienthienngt1 <tienthienngt2@gmail.com>
2023-01-13 21:22:45 +0700

    change avatar

--- components/commons/ContractAddress.component.tsx
+++ components/commons/ContractAddress.component.tsx
@@ -14,8 +14,7 @@ const ContractAddress = () => {
 				>
 					<Box sx={{ wordBreak: "break-word", padding: "10px" }}>
 						<Typography align="center" variant="h6">
-							Contract Address (ERC-20):
-							0x9aB87a443218aAA07Bfc6eDD5A8f2a28eB92a83C
+							Contract Address (ERC-20): Comming soon
 						</Typography>
 					</Box>
 				</Container>
